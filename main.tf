40ad10465a2e6974bd48f4e21ea5bd95c17b00a4
jetweed <67114704+jetweed@users.noreply.github.com>
2020-07-28 19:16:13 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,13 +3,6 @@ provider "azurerm" {
   version = "~>1.31"
 }
 
-subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-
-  features {}
-
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
